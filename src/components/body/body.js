3fb41d918ee8ad05717e458c8d91c67e0249273e
Milan Stojanovic <milan.stojanovic@naissus.tech>
2022-01-29 21:21:09 +0100

    Initial commit

---
+++ src/components/body/body.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Blocks from "./blocks/block"
+import "./body.css"
+function body() {
+  return (
+    <div className="body">
+      <Blocks/>
+    </div>
+  );
+}
+
+export default body;
